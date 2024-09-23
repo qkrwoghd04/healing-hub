6b9d73ba4ceb1eeb143b7e68f23684a639bfa611
JDEV <qkrwoghd04@naver.com>
2024-09-23 15:25:48 +0900

    Modified profile

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -6,28 +6,29 @@ const Profile = () => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-        <View style={[styles.imageContainer, {width: '100%', alignItems: 'center'}]}>
+        <View style={[styles.imageContainer, { width: '100%', alignItems: 'center' }]}>
           <Image
             source={require('../../assets/images/shop.jpg')}
             style={styles.storeImage}
           />
         </View>
         <View style={styles.infoContainer}>
-          <InfoItem label="가게 이름" value="힐링 허브" />
+          <InfoItem label="매장 소개" value="힐링허브는 회원님의 환하게 웃는 모습을 기대하며 최고의 제품을 최저가로 공급합니다" />
           <View style={styles.separator} />
-          <InfoItem 
-            label="주소" 
-            value="서울특별시 중구 청계천로 246 평화시장 2층 나 169호"
-          />
-          <View style={styles.separator} />
-          <InfoItem 
-            label="전화번호" 
+          <InfoItem
+            label="전화번호"
             value="010-4040-1669"
           />
           <View style={styles.separator} />
-          <InfoItem label="영업시간" value="평일 9:30 - 18:00, 일요일만 휴무" />
+          <InfoItem label="영업시간" value="평일 9:30 - 17:00" />
+          <Text style={[styles.textRed, {color: "red", fontSize: wp('5%'), marginBottom: hp('1%')}]}>일요일만 휴무</Text>
           <View style={styles.separator} />
-          <InfoItem label="소개" value="최고 품질의 건강식품만을 판매하는 전문점입니다. 고객 여러분의 건강을 지켜드리겠습니다." />
+          <InfoItem
+            label="오시는법"
+            value="1호선 동대문역 8번출구, 종로 5가역 6번출구"
+          />
+          <Text style={[styles.text, {fontSize: wp('4.5%'), fontFamily: 'Pretendard-Light',marginBottom: hp('1%')}]}>2호선, 3호선, 5호선, 4호선 동대문역사문화공원역 14번 출구 </Text>
+          <Text style={[styles.textRed, {color: "red", fontSize: wp('5%'), marginBottom: 5}]}>평화시장방면 2층 169호</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -46,11 +47,11 @@ const InfoItem = ({ label, value, isAddress = false, isPhoneNumber = false }) =>
 
   return (
     <View style={styles.infoItem}>
-      <Text style={[styles.label, {fontFamily: 'Pretendard-Medium'}]}>{label}:</Text>
+      <Text style={[styles.label, { fontFamily: 'Pretendard-Medium' }]}>{label}:</Text>
       {isAddress || isPhoneNumber ? (
         <TouchableOpacity onPress={handlePress}>
           <Text style={[
-            styles.value, 
+            styles.value,
             {
               fontFamily: 'Pretendard-Light',
               color: isAddress || isPhoneNumber ? '#0000FF' : '#000',
@@ -61,7 +62,7 @@ const InfoItem = ({ label, value, isAddress = false, isPhoneNumber = false }) =>
           </Text>
         </TouchableOpacity>
       ) : (
-        <Text style={[styles.value, {fontFamily: 'Pretendard-Light'}]}>{value}</Text>
+        <Text style={[styles.value, { fontFamily: 'Pretendard-Light' }]}>{value}</Text>
       )}
     </View>
   );
@@ -93,9 +94,9 @@ const styles = StyleSheet.create({
     fontSize: wp('4.5%'), // 화면 크기에 맞게 폰트 크기 조정
   },
   separator: {
-    height: 1, 
-    backgroundColor: '#ccc', 
-    opacity: 0.5, 
+    height: 1,
+    backgroundColor: '#ccc',
+    opacity: 0.5,
     width: '100%',
   },
 });
