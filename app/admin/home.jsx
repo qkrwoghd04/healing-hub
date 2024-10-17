8bf707b689d6757e320e63b34aca30ee08c7be19
JDEV <qkrwoghd04@naver.com>
2024-10-17 14:40:21 +0900

    updated admin home

--- app/admin/home.jsx
+++ app/admin/home.jsx
@@ -3,63 +3,72 @@ import { View, Text, Image, TouchableOpacity, StyleSheet, ScrollView, TextInput,
 import { useRouter } from 'expo-router';
 import { useProducts } from '../../api/ProductContext';
 import * as ImagePicker from 'expo-image-picker';
+import { Entypo, Octicons } from '@expo/vector-icons';
+
+
+//Components
+import AdminHeader from '../../components/AdminHeader'
+import { FormatPrice } from '../../components/FormatPrice'
 
 const AdminHome = () => {
   const { products, addNewProduct, removeProduct } = useProducts();
   const [modalVisible, setModalVisible] = useState(false);
   const [newProductName, setNewProductName] = useState('');
+  const [newProductShortName, setNewProductShortName] = useState('');
   const [newProductPrice, setNewProductPrice] = useState('');
   const [newProductDetail, setNewProductDetail] = useState('');
+  const [newProductPopularity, setNewProductPopularity] = useState('');
   const [newProductImage, setNewProductImage] = useState(null);
-  const router = useRouter();
 
+
+  //
   const addProduct = async () => {
-    if (products.length < 13) {
-      if (!newProductName || !newProductPrice || !newProductImage || !newProductDetail) {
-        Alert.alert('입력 오류', '모든 필드를 입력해주세요.');
-        return;
-      }
+    if (!newProductName || !newProductPrice || !newProductImage || !newProductDetail) {
+      Alert.alert('입력 오류', '모든 필드를 입력해주세요.');
+      return;
+    }
 
-      const formData = new FormData();
-      formData.append('name', newProductName);
-      formData.append('price', newProductPrice);
-      formData.append('description', newProductDetail);
-      formData.append('image', {
-        uri: newProductImage.uri,
-        type: 'image/jpeg',
-        name: 'product.jpg',
-      });
+    const formData = new FormData();
+    formData.append('name', newProductName);
+    formData.append('price', newProductPrice);
+    formData.append('description', newProductDetail);
+    formData.append('shortName', newProductShortName);
+    formData.append('popularity', newProductPopularity);
+    formData.append('image', {
+      uri: newProductImage.uri,
+      type: 'image/jpeg',
+      name: 'product.jpg',
+    });
 
-      try {
-        await addNewProduct(formData);
-        resetForm();
-        setModalVisible(false);
-        Alert.alert('성공', '새 상품이 추가되었습니다.');
-      } catch (error) {
-        console.error('Error adding product:', error);
-        Alert.alert('오류', '상품 추가에 실패했습니다.');
-      }
-    } else {
-      Alert.alert('최대 개수 초과', '최대 12개의 상품만 등록할 수 있습니다.');
+    try {
+      await addNewProduct(formData);
+      resetForm();
+      setModalVisible(false);
+      Alert.alert('성공', '새 상품이 추가되었습니다.');
+    } catch (error) {
+      console.error('Error adding product:', error);
+      Alert.alert('오류', '상품 추가에 실패했습니다.');
     }
   };
 
   const resetForm = () => {
     setNewProductName('');
+    setNewProductShortName('');
     setNewProductPrice('');
     setNewProductDetail('');
+    setNewProductPopularity('');
     setNewProductImage(null);
   };
 
   const confirmRemoveProduct = (id) => {
-    if(products.length > 1){
+    if (products.length > 1) {
       Alert.alert(
         "상품 삭제",
         "상품을 삭제하시겠습니까?",
         [
           { text: "취소", style: "cancel" },
-          { 
-            text: "삭제", 
+          {
+            text: "삭제",
             onPress: async () => {
               try {
                 await removeProduct(id);
@@ -82,7 +91,7 @@ const AdminHome = () => {
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [4, 3],
-      quality: 0.3,
+      quality: 0.5,
     });
 
     if (!result.canceled) {
@@ -90,47 +99,42 @@ const AdminHome = () => {
     }
   };
 
-  const navigateToHome = () => {
-    router.push('/(app)');
-  };
-
-  const formatPrice = (price) => {
-    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "원";
-  };
 
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <View style={styles.header}>
-        <Text style={styles.title}>관리자 홈</Text>
-        <TouchableOpacity onPress={navigateToHome} style={styles.homeButton}>
-          <Image source={require('../../assets/icons/home.png')} style={styles.homeIcon} />
-        </TouchableOpacity>
-      </View>
-      <ScrollView contentContainerStyle={styles.scrollViewContent}>
-        <View style={styles.container}>
-          {products.map((product) => (
-            <View key={product.id} style={styles.productContainer}>
-            <Image 
-              source={{ uri: product.image }}
-              style={styles.productImage} 
-            />
-            <View style={styles.verticalLine} />
-            <View style={styles.productInfo}>
-              <Text style={styles.productName}>{product.name}</Text>
-              <Text style={styles.productPrice}>{formatPrice(product.price)}</Text>
+    <SafeAreaView className="w-full h-full">
+      {/* Header */}
+      <AdminHeader />
+      {/* Product Info */}
+      <ScrollView className="w-full h-full mx-2">
+        {products.map((product) => (
+          <View key={product.id} className="w-full h-[40%] flex flex-row justify-start items-center my-1">
+            {/* Product Container */}
+            <View className="w-[15%] h-full">
+              <Image
+                source={{ uri: product.image }}
+                className="w-full h-full rounded-lg"
+              />
             </View>
-            <TouchableOpacity onPress={() => confirmRemoveProduct(product.id)} style={styles.deleteButton}>
-              <Image source={require('../../assets/icons/delete.png')} style={styles.deleteIcon} />
+
+            {/* Product Info */}
+            <View className="w-[70%] h-full flex justify-between items-start border-x-[0.3px] px-1 mx-1">
+              <Text className="font-Pretendard-Medium">{product.name}</Text>
+              <Text>{FormatPrice(product.price)}</Text>
+            </View>
+            {/* Delete Button */}
+            <TouchableOpacity onPress={() => confirmRemoveProduct(product.id)}>
+              <Entypo name="cross" size={30} color="gray" />
             </TouchableOpacity>
           </View>
-          ))}
-        </View>
+        ))}
       </ScrollView>
-      
-      <TouchableOpacity style={styles.addButton} onPress={() => setModalVisible(true)}>
-        <Text style={styles.addButtonText}>상품 추가</Text>
+
+      {/* Add Button */}
+      <TouchableOpacity className="w-12 h-12 absolute bottom-10 right-5 bg-gray-600 flex justify-center items-center rounded-lg" onPress={() => setModalVisible(true)}>
+        <Octicons name="diff-added" size={35} color="white" />
       </TouchableOpacity>
 
+      {/* Modal */}
       <Modal
         animationType="slide"
         transparent={true}
@@ -138,41 +142,56 @@ const AdminHome = () => {
         onRequestClose={() => setModalVisible(false)}
       >
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          <View style={styles.modalContainer}>
-            <View style={styles.modalContent}>
-              <TouchableOpacity style={styles.imagePicker} onPress={pickImage}>
-                {newProductImage ? (
-                  <Image source={{ uri: newProductImage.uri }} style={styles.newProductImage} />
-                ) : (
-                  <Text style={styles.imagePickerText}>이미지 선택</Text>
-                )}
-              </TouchableOpacity>
-              <TextInput
-                style={styles.input}
-                placeholder="제품명"
-                value={newProductName}
-                onChangeText={setNewProductName}
-              />
-              <TextInput
-                style={styles.input}
-                placeholder="가격"
-                value={newProductPrice}
-                onChangeText={setNewProductPrice}
-                keyboardType="numeric"
-              />
-              <TextInput
-                style={[styles.input, styles.textArea]}
-                placeholder="상품 상세 설명"
-                value={newProductDetail}
-                onChangeText={setNewProductDetail}
-                multiline
-              />
-              <TouchableOpacity style={styles.modalButton} onPress={addProduct}>
-                <Text style={styles.modalButtonText}>추가</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={[styles.modalButton, styles.cancelButton]} onPress={() => setModalVisible(false)}>
-                <Text style={styles.modalButtonText}>취소</Text>
-              </TouchableOpacity>
+          <View className="flex-1 justify-center items-center" style={{ backgroundColor: 'rgba(68, 63, 61, 0.6)' }}>
+            <View className="bg-white rounded-lg w-[90%] h-[60%] p-4 flex flex-col justify-between items-center">
+              {/* Modal Header */}
+              <View className="w-full h-1/4 flex flex-row justify-start items-center gap-x-2">
+                <TouchableOpacity className="flex justify-center items-center rounded-lg w-[30%] h-full border-[1.5px] border-gray-400" onPress={pickImage}>
+                  {newProductImage ? (
+                    <Image source={{ uri: newProductImage.uri }} className="w-full h-full rounded-lg" />
+                  ) : (
+                    <Text className="text-[#847958] text-[16px] font-Pretendard-Medium">이미지 선택</Text>
+                  )}
+                </TouchableOpacity>
+
+                <View className="flex flex-col justify-between items-center w-[65%] h-full gap-y-2">
+                  <TextInput
+                    className="bg-white w-full h-[40%] rounded-lg border-[1.5px] border-gray-400 p-2"
+                    placeholder="제품명"
+                    value={newProductName}
+                    onChangeText={setNewProductName}
+                  />
+                  <TextInput
+                    className="bg-white w-full h-[40%] rounded-lg border-[1.5px] border-gray-400 p-2"
+                    placeholder="가격"
+                    value={newProductPrice}
+                    onChangeText={setNewProductPrice}
+                    keyboardType="numeric"
+                  />
+                </View>
+              </View>
+
+              {/* Modal Body */}
+              <View className="w-full h-2/4 border-2 border-gray-200 rounded-md">
+                <TextInput
+                  className="w-full p-2"
+                  placeholder="상품 상세 설명"
+                  value={newProductDetail}
+                  onChangeText={setNewProductDetail}
+                  multiline
+                />
+                
+              </View>
+              
+              {/* Modal Bottom */}
+              <View className="flex flex-row justify-between items-center w-full h-[10%]">
+                <TouchableOpacity className="bg-[#847958] flex-1 h-[90%] justify-center items-center rounded-md mr-1" onPress={addProduct}>
+                  <Text className="text-white text-xl font-Pretendard-Medium">추가</Text>
+                </TouchableOpacity>
+                <TouchableOpacity className="bg-[#954F4C] flex-1 h-[90%] justify-center items-center rounded-md ml-1" onPress={() => setModalVisible(false)}>
+                  <Text className="text-white text-xl font-Pretendard-Medium">취소</Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
         </TouchableWithoutFeedback>
@@ -180,188 +199,5 @@ const AdminHome = () => {
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingHorizontal: 20,
-    paddingVertical: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: '#e9ede6',
-    backgroundColor: '#fff',
-    marginTop: 20
-  },
-  title: {
-    fontSize: 38,
-    fontWeight: 'bold',
-    color: '#443F3D',
-    fontFamily: 'Typography-Times-Regular',
-  },
-  homeButton: {
-    padding: 8,
-    borderRadius: 10,
-  },
-  homeIcon: {
-    width: 24,
-    height: 24,
-    tintColor: '#443F3D',
-  },
-  scrollViewContent: {
-    flexGrow: 1,
-    paddingBottom: 100,
-  },
-  container: {
-    padding: 20,
-  },
-  productContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    marginBottom: 15,
-    padding: 15,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 3.84,
-    elevation: 3,
-  },
-  verticalLine: {
-    height: '80%',
-    width: 1,
-    backgroundColor: 'rgba(132, 121, 88, 0.2)', // #847958 with 20% opacity
-    marginHorizontal: 10,
-  },
-  productImage: {
-    width: 80,
-    height: 80,
-    borderRadius: 10,
-  },
-  productInfo: {
-    flex: 1,
-    marginLeft: 5,
-  },
-  productName: {
-    fontSize: 18,
-    fontWeight: '600',
-    color: '#443F3D',
-    marginBottom: 4,
-    fontFamily: 'Pretendard-Medium',
-  },
-  productPrice: {
-    fontSize: 16,
-    color: '#847958',
-    fontFamily: 'Pretendard-Medium',
-  },
-  deleteButton: {
-    padding: 8,
-    borderRadius: 8,
-  },
-  deleteIcon: {
-    width: 25,
-    height: 25,
-    tintColor: '#954F4C',
-  },
-  addButton: {
-    position: 'absolute',
-    bottom: 20,
-    right: 20,
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#954F4C',
-    borderRadius: 15,
-    paddingVertical: 12,
-    paddingHorizontal: 20,
-    shadowColor: "#4299E1",
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.3,
-    shadowRadius: 5,
-    elevation: 6,
-    marginBottom: 25
-  },
-  addIcon: {
-    width: 20,
-    height: 20,
-    tintColor: '#FFFFFF',
-    marginRight: 8,
-  },
-  addButtonText: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: '700',
-    fontFamily: 'Pretendard-Medium',
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(68, 63, 61, 0.6)',
-  },
-  modalContent: {
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    padding: 24,
-    width: '90%',
-    maxHeight: '80%',
-  },
-  imagePicker: {
-    width: 120,
-    height: 120,
-    borderWidth: 2,
-    borderColor: '#847958',
-    borderRadius: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignSelf: 'center',
-    marginBottom: 24,
-    overflow: 'hidden',
-  },
-  imagePickerText: {
-    color: '#847958',
-    fontSize: 16,
-    fontFamily: 'Pretendard-Medium',
-  },
-  newProductImage: {
-    width: '100%',
-    height: '100%',
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#e9ede6',
-    borderRadius: 8,
-    padding: 12,
-    marginBottom: 16,
-    fontSize: 16,
-    backgroundColor: '#fff',
-    fontFamily: 'Pretendard-Medium',
-  },
-  textArea: {
-    height: 100,
-    textAlignVertical: 'top',
-  },
-  modalButton: {
-    backgroundColor: '#847958',
-    padding: 14,
-    borderRadius: 8,
-    alignItems: 'center',
-    marginTop: 12,
-  },
-  cancelButton: {
-    backgroundColor: '#954F4C',
-    marginTop: 12,
-  },
-  modalButtonText: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: '600',
-    fontFamily: 'Pretendard-Medium',
-  },
-});
 
 export default AdminHome;
