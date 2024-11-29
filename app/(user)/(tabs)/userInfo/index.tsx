4ba02f52da54c0e671f26768c3debeb30190e751
JDEV <qkrwoghd04@naver.com>
2024-11-29 23:41:22 +0900

    [#EXPO007] Custom hook 으로 Pressable 요소 클릭 시 에니메이션 효과 구현-COMPLETE-ModifyAppIcon

--- app/(user)/(tabs)/userInfo/index.tsx
+++ app/(user)/(tabs)/userInfo/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { SafeAreaView, Text, View, Pressable } from 'react-native';
 import { Image } from 'expo-image';
-import { login } from '@react-native-seoul/kakao-login';
+// import { login } from '@react-native-seoul/kakao-login';
 
 const UserInfo = () => {
   return (
     <SafeAreaView className="flex-1">
       <View className="flex-1 justify-center items-center">
         <Text>index</Text>
-        <Pressable onPress={login}>
+        {/* <Pressable onPress={login}>
           <Image
             source="https://k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg"
             style={{
@@ -17,7 +17,7 @@ const UserInfo = () => {
             }}
             contentFit="contain"
           />
-        </Pressable>
+        </Pressable> */}
       </View>
     </SafeAreaView>
   );
